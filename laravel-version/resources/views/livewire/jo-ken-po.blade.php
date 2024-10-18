c9eafa2397e265011b0c8578f0366b6bcbe4855f
= <augustodemeloh@protonmail.com>
2024-10-18 00:06:51 -0300

    refactor: modify jokenpo style-attribute-based style into tailwind classes

--- laravel-version/resources/views/livewire/jo-ken-po.blade.php
+++ laravel-version/resources/views/livewire/jo-ken-po.blade.php
@@ -1,38 +1,38 @@
-<div style="display: flex; flex-direction: column; padding: 0 50px">
-    <div style="display: flex; justify-content: center">
-        <div class="player-wrapper" style="position: relative">
-            <p style="position: absolute; left: 50%; transform: translateX(-50%); font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white; font-size: 20px;">
+<div class="flex flex-col px-12">
+    <div class="flex justify-center">
+        <div class="relative player-wrapper">
+            <p class="absolute font-sans text-lg text-white transform -translate-x-1/2 left-1/2">
                 Player
             </p>
-            <div class="player-hand-wrapper" style="width: 300px; height: 300px; transform: rotate(90deg); display: flex; justify-content: center; align-items: center;">
-                <img @if($playerHand) src="{{ $playerHand }}" @endif class="player-hand hand" style="width: 75%; height: 75%;" />
+            <div class="flex items-center justify-center transform rotate-90 player-hand-wrapper w-72 h-72">
+                <img @if($playerHand) src="{{ $playerHand }}" @endif class="w-3/4 player-hand hand h-3/4" />
             </div>
         </div>
 
-        <div class="player-wrapper" style="position: relative">
-            <p style="position: absolute; left: 50%; transform: translateX(-50%); font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white; font-size: 20px;">
+        <div class="relative player-wrapper">
+            <p class="absolute font-sans text-lg text-white transform -translate-x-1/2 left-1/2">
                 IA
             </p>
-            <div class="ia-hand-wrapper" style="width: 300px; height: 300px; transform: rotate(-90deg); display: flex; justify-content: center; align-items: center;">
-                <img @if($iaHand) src="{{ $iaHand }}" @endif class="ia-hand hand" style="width: 75%; height: 75%;" />
+            <div class="flex items-center justify-center transform -rotate-90 ia-hand-wrapper w-72 h-72">
+                <img @if($iaHand) src="{{ $iaHand }}" @endif class="w-3/4 ia-hand hand h-3/4" />
             </div>
         </div>
     </div>
 
-    <div class="jokenpo-btns-wrapper" style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 50px">
-        <button type="button" wire:click="play('rock')" class="rock-btn">rock</button>
-        <button type="button" wire:click="play('paper')" class="paper-btn">paper</button>
-        <button type="button" wire:click="play('scissors')" class="scissors-btn">scissors</button>
+    <div class="grid grid-cols-3 gap-12 mt-8 jokenpo-btns-wrapper">
+        <button type="button" wire:click="play('rock')" class="text-black bg-gray-100 rounded shadow rock-btn hover:bg-gray-200">rock</button>
+        <button type="button" wire:click="play('paper')" class="text-black bg-gray-100 rounded shadow paper-btn hover:bg-gray-200">paper</button>
+        <button type="button" wire:click="play('scissors')" class="text-black bg-gray-100 rounded shadow scissors-btn hover:bg-gray-200">scissors</button>
     </div>
 
-    <div class="result-wrapper" style="display: flex; justify-content: center">
+    <div class="flex justify-center mt-8 result-wrapper">
         <p
-        @style([
-            'font-family: Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
-            'font-size: 20px',
-            'color: lightgreen' => $resultText === 'Player won!',
-            'color: lightcoral' => $resultText === 'IA won!',
-            'color: yellow' => $resultText === 'Draw!',
+        @class([
+            'font-sans',
+            'text-lg',
+            'text-lightgreen' => $resultText === 'Player won!',
+            'text-lightcoral' => $resultText === 'IA won!',
+            'text-yellow' => $resultText === 'Draw!',
             ])
         class="resultText"
         >
