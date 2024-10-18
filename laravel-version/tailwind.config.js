32432079daee44f8c56ada04d84ff5b63f0e5a9b
= <augustodemeloh@protonmail.com>
2024-10-18 00:05:17 -0300

    feat: add tailwindcss

--- laravel-version/tailwind.config.js
+++ laravel-version/tailwind.config.js
@@ -1,18 +1,19 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                lightgreen: "#90ee90",
+                lightcoral: "#f08080",
+                yellow: "#ffff00",
             },
         },
     },
