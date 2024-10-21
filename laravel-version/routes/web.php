2ea20fd3a63003081abb93b63473205e9b545df5
= <augustodemeloh@protonmail.com>
2024-10-21 08:41:08 -0300

    feat: add Leadership component

--- laravel-version/routes/web.php
+++ laravel-version/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\JoKenPo;
 use App\Livewire\JoKenPoXLuckMode;
+use App\Livewire\Leadership;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
@@ -17,5 +18,7 @@ Route::view('profile', 'profile')
 Route::get('/play/simple-mode', JoKenPo::class)->name('play-simple-mode');
 
 Route::get('/play/x-luck-mode', JoKenPoXLuckMode::class)->name('play-x-luck-mode');
+
+Route::get('/leadership', Leadership::class)->name('leadership');
 
 require __DIR__.'/auth.php';
